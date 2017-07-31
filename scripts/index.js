4bad756344e1bb9bdc3863d8025f9c8251108392
Michael Daryl P. Mayo <michaeldaryl.code@gmail.com>
2017-07-31 23:05:34 +0800

    Adds final touches

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
+    createGrid(16); // creates the grid;
+
+    $('button').click(function() {
+      $('.pixel').removeClass('color'); // reset the color
+    });
+});
+
+
+$(document).on("mouseenter", ".pixel", function() {
 
   /**
-   * This functions creates a grid of divs.
-   * Receives the variable gridLength that defines the size of the grid.
+   * Changes the color of the pixel when the mouse pointer entered
+   * the pixel area.
    */
-  function createGrid(gridLength) {
-    for (var i = 0; i < gridLength; i++) {
-      var $row = $('<div class="row"></div>'); // creates a pixel row
-
-      for (var j = 0; j < gridLength; j++) {
-
-        var $pixel = $('<div class="pixel"></div>');  // creates a pixel
-
-        $row.append($pixel); // add pixel to row
-      }
-
-      $('.container').append($row); // add row to container
-    }
-  }
-
-  createGrid(16);
+  $(this).addClass("color");
 });
+
+
+/**
+ * This functions creates a grid of divs.
+ * Receives the variable gridLength that defines the size of the grid.
+ */
+function createGrid(gridLength) {
+  for (var i = 0; i < gridLength; i++) {
+    var $row = $('<div class="row"></div>'); // creates a pixel row
+
+    for (var j = 0; j < gridLength; j++) {
+
+      var $pixel = $('<div class="pixel"></div>'); // creates a pixel
+
+      $row.append($pixel); // add pixel to row
+    }
+
+    $('.container').append($row); // add row to container
+  }
+};
